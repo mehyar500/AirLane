5c936035eb0a0f50f7f936c03845feb7984b1b40
mehyar500 <mehyar500@gmail.com>
2017-11-18 10:12:44 -0500

    MDL UI Library

---
+++ client/src/components/mdlLight.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import { CommandCursor } from "./C:/Users/mehya/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/mongodb";
+
+
+class mdlLight extends Component {
+
+
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default mdlLight;
