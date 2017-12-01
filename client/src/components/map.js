3b188a3c42289fbac7ea8d5bb19a84ae2aad6412
mehyar500 <mehyar500@gmail.com>
2017-11-30 22:09:40 -0500

    map

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,19 +1,22 @@
 import React, {Component} from "react";
+import { GoogleMap, Marker } from "react-google-maps";
 
 export default class Map extends Component {
     constructor(props) {
-    super(props);
-    this.state = {};
+        super(props);
+        this.state = {
+            center: {lat: null, lng: null},
+            zoom: null
+        };
     }
 
     componentDidMount() {
-        this.map = new google.maps.Map(
-            this.refs.map,{
-                center: EIFFEL_TOWER_POSITION,
-                zoom: 16
-            }
-        );
+        this.map = new google.maps.Map(this.refs.map, {
+            center: { lat: -28.643387, lng: 153.612224 },
+            zoom: 16
+        });
     }
+
 
     render() {
     return <div ref="map" style={mapStyle} ref="map">
