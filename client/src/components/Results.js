42f5f6deaf1f7764c7f7450c78fc8f6fe6ebbe81
cmoxey8719 <kirbymox@yahoo.com>
2017-11-30 21:18:32 -0500

    added pages

---
+++ client/src/components/Results.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+
+class Results extends Component {
+
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<h3>Results</h3>
+						<p> reults here </p>
+						<div className="results"></div>
+					</div>
+					<div className="col-md-6">
+						<h3> Google Maps here </h3>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Results;
