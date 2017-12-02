c0c4f427fb235160f25c5f213aac4344c943678b
cmoxey8719 <kirbymox@yahoo.com>
2017-12-01 23:28:55 -0500

    seperated results and maps box

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Appbody.css";
 
 
 class Results extends Component {
@@ -7,13 +8,15 @@ class Results extends Component {
 		return (
 			<div className="container">
 				<div className="row">
-					<div className="col-md-6">
+					<div className="col-md-5 one">
 						<h3>Results</h3>
 						<p> reults here </p>
 						<div className="results"></div>
 					</div>
-					<div className="col-md-6">
+					<div className="col-md-1"></div>
+					<div className="col-md-6 one">
 						<h3> Google Maps here </h3>
+						<div className="google"></div>
 					</div>
 				</div>
 			</div>
