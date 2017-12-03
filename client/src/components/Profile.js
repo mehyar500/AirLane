0bdfed23777663968fb77871c1074629c65c7e8c
mehyar500 <mehyar500@gmail.com>
2017-12-03 17:11:32 -0500

    profile updated

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -11,100 +11,101 @@ import { Button,
   Col,
   Row
 } from "react-bootstrap";
-
+import API from "../Utils/API";
 
 
   
   class Profile extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value: ''};
-  
+      this.state = { showModal: false, firstName: "", lastName: "", email: "", password: "" };
+
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <div className="container one">
-        <div className="row">     
-        <div className="col-md-12">
-              <h1>Your Profile</h1>
-        </div>
-        </div>
-        <div className="row">
-        <div className="col-md-12"></div>
-        </div>
-        <div className="row">
-        <div className="col-md-12"></div>
-        </div>
-        <div className="row">
-        <div className="col-md-12"></div>
-        </div>
-        <Form horizontal onSubmit={this.handleSubmit}>
-        <FormGroup>
-        <Col componentClass={ControlLabel} md={2}>
-          First Name
-        </Col>
-        <Col md={8}>
-        <FormControl name="firstName" type="text" placeholder="ex: Tom" value={this.state.value} onChange={this.handleChange} />
-        </Col>
-      </FormGroup>
-      <FormGroup>
-        <Col componentClass={ControlLabel} md={2}>
-          Last Name
-        </Col>
-        <Col md={8}>
-        <FormControl name="lastName" type="text" placeholder="ex: Smith" value={this.state.value} onChange={this.handleChange} />
-        </Col>
-      </FormGroup>
-      <FormGroup controlId="formHorizontalEmail">
-        <Col componentClass={ControlLabel} md={2}>
-          Email Address
-        </Col>
-        <Col md={8}>
-        <FormControl name="emailAddress" type="email" placeholder="ex: TSmith@xxx.com" value={this.state.value} onChange={this.handleChange} />
-        </Col>
-      </FormGroup>
-      <FormGroup>
-        <Col componentClass={ControlLabel} md={2}>
-          Jet Model
-        </Col>
-        <Col md={8}>
-        <FormControl name="jetModel" type="text" placeholder="ex: Boeing Business" value={this.state.value} onChange={this.handleChange} />
-        </Col>
-      </FormGroup>
-      <FormGroup>
-        <Col componentClass={ControlLabel} md={2}>
-          Jet Identification
-        </Col>
-        <Col md={8}>
-        <FormControl name="jstId" type="text" placeholder="ex: B234H569F33" value={this.state.value} onChange={this.handleChange} />
-        </Col>
-      </FormGroup>
-      <FormGroup>
-        <Col componentClass={ControlLabel} md={2}>
-          Jet Location
-        </Col>
-        <Col md={8}>
-        <FormControl name="jstLocation" type="text" placeholder="ex: San Jose, CA" value={this.state.value} onChange={this.handleChange} />
-        </Col>
-      </FormGroup>
-          <input type="submit" value="Submit" />
-        </Form>
-          
-          </div>
 
-      );
+    // Handles updating component state when the user types into the input field
+    handleChange = event => {
+      // console.log(event.target.value);
+      const { name, value } = event.target;
+      this.setState({ [name]: value });
+    };
+
+    handleSubmit(event) {
+      const { name, value } = event.target;
+      alert("A " + [name] + " was submitted: " + value);
+      event.preventDefault();
+      
+    }
+
+    render() {
+      return <div className="container one">
+          <div className="row">
+            <div className="col-md-12">
+              <h1>Your Profile</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12" />
+          </div>
+          <div className="row">
+            <div className="col-md-12" />
+          </div>
+          <div className="row">
+            <div className="col-md-12" />
+          </div>
+          <Form horizontal onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Col componentClass={ControlLabel} md={2}>
+                First Name
+              </Col>
+              <Col md={8}>
+                <FormControl name="firstName" type="text" placeholder="ex: Tom" value={this.state.value} onChange={this.handleChange} />
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col componentClass={ControlLabel} md={2}>
+                Last Name
+              </Col>
+              <Col md={8}>
+                <FormControl name="lastName" type="text" placeholder="ex: Smith" value={this.state.value} onChange={this.handleChange} />
+              </Col>
+            </FormGroup>
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} md={2}>
+                Email Address
+              </Col>
+              <Col md={8}>
+                <FormControl name="emailAddress" type="email" placeholder="ex: TSmith@xxx.com" value={this.state.value} onChange={this.handleChange} />
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col componentClass={ControlLabel} md={2}>
+                Jet Model
+              </Col>
+              <Col md={8}>
+                <FormControl name="jetModel" type="text" placeholder="ex: Boeing Business" value={this.state.value} onChange={this.handleChange} />
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col componentClass={ControlLabel} md={2}>
+                Jet Identification
+              </Col>
+              <Col md={8}>
+                <FormControl name="jstId" type="text" placeholder="ex: B234H569F33" value={this.state.value} onChange={this.handleChange} />
+              </Col>
+            </FormGroup>
+            <FormGroup>
+              <Col componentClass={ControlLabel} md={2}>
+                Jet Location
+              </Col>
+              <Col md={8}>
+                <FormControl name="jstLocation" type="text" placeholder="ex: San Jose, CA" value={this.state.value} onChange={this.handleChange} />
+              </Col>
+            </FormGroup>
+            <input type="submit" value="Submit" />
+          </Form>
+        </div>;
     }
   }
 
