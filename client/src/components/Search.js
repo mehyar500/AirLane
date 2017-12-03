cac93647b27b685883b5465d2544d9865f67808c
mehyar500 <mehyar500@gmail.com>
2017-12-03 17:00:46 -0500

    map

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,46 +1,93 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup,FormControl, InputGroup } from "react-bootstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  FormControl,
+  InputGroup,
+  Modal
+} from "react-bootstrap";
 
 class Search extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = { showModal: false };
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
   }
 
-  render() {
-      return <div className="container">
-          <div className="row">
-            <div className="col-md-3"></div>
-            <div className="col-md-6">
-              <Form>
-                <FormGroup row>
+  open = () => {
+    this.setState({ showModal: true });
+  };
 
-                  <InputGroup type="text" name="firstName" id="firstName" placeholder="First Name">
-                    <InputGroup.Addon>Location</InputGroup.Addon>
-                    <FormControl type="text" placeholder="First Name" />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <InputGroup type="text" name="NumPass" id="NumPass">
-                    <InputGroup.Addon>Number of Passengers</InputGroup.Addon>
-                    <FormControl type="text" placeholder="Number of Passengers" />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <InputGroup type="text" name="AirType" id="AirType">
-                    <InputGroup.Addon>Type of Airplane</InputGroup.Addon>
-                    <FormControl type="boolean" placeholder="Type of Airplane" />
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup>
-                  <Button>Save</Button>
-                </FormGroup>
-              </Form>
+  close = () => {
+    this.setState({ showModal: false });
+  };
+
+  // Handles updating component state when the user types into the input field
+  handleChange = event => {
+    // console.log(event.target.value);
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  //when the form is submitted, use the API to save a user
+  handleSubmit = event => {
+    event.preventDefault();
+    // const { firstName, lastName, password } = this.state;
+    // API.saveUser(firstName, lastName, password)
+    //   .then(console.log(firstName, lastName, password))
+    //   .catch(err => console.log("Signup.js API call " + err));
+  };
+
+  render() {
+    return <div className="container" onClick={this.open}>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Registeration</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="row">
+              <div className="col-md-3" />
+              <div className="col-md-6">
+                <Form>
+                  <FormGroup row>
+                    <InputGroup type="text" name="firstName" id="firstName" placeholder="First Name">
+                      <InputGroup.Addon>Location</InputGroup.Addon>
+                      <FormControl type="text" placeholder="First Name" />
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup>
+                    <InputGroup type="text" name="NumPass" id="NumPass">
+                      <InputGroup.Addon>
+                        Number of Passengers
+                      </InputGroup.Addon>
+                      <FormControl type="text" placeholder="Number of Passengers" />
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup>
+                    <InputGroup type="text" name="AirType" id="AirType">
+                      <InputGroup.Addon>Type of Airplane</InputGroup.Addon>
+                      <FormControl type="boolean" placeholder="Type of Airplane" />
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup>
+                    <Button>Save</Button>
+                  </FormGroup>
+                </Form>
+              </div>
+              <div className="col-md-3" />
             </div>
-            <div className="col-md-3"></div>
-          </div>
-        </div>;
-    }
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>;
+  }
 }
 
 export default Search;
+
